c927422bd2633e24a329fccec8abe817c57338d8
Trần Đức An <110535543+06babyshark06@users.noreply.github.com>
2025-04-22 23:54:15 +0700

    fix auth

--- src/auth/authController.js
+++ src/auth/authController.js
@@ -8,7 +8,7 @@ const register = async (req, res) => {
   try {
     const user = await User.find({ name: username } || { email });
     console.log(user);
-    if (user) return res.status(400).json({ message: "User already exists" });
+    if (user.length) return res.status(400).json({ message: "User already exists" });
     const hashed = await bcrypt.hash(password, 10);
     const newUser = await User.create({
       name: username,
